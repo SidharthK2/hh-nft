d1554f98c71607b9a56c6e86007d47c1ef9b926a
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-27 20:43:00 +0530

    Add unit test for constructor, token uri getter and mint function

--- test/basicNft.test.js
+++ test/basicNft.test.js
@@ -1,2 +1,41 @@
 const { assert, expect } = require("chai");
-const { getNamedAccounts, deployments, ethers, network } = reqiure("hardhat");
+const { getNamedAccounts, deployments, ethers, network } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../helper-hardhat-config");
+const { deploy, log } = deployments;
+
+!developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("FundMe", function () {
+      let basicNft;
+      beforeEach(async () => {
+        let { deployer } = await getNamedAccounts();
+        await deployments.fixture(["basicnft"]);
+        basicNft = await ethers.getContract("BasicNFT", deployer);
+      });
+
+      describe("constructor", function () {
+        it("sets the name and symbol and counter correctly", async () => {
+          const name = await basicNft.name();
+          const symbol = await basicNft.symbol();
+          const counter = await basicNft.getTokenCounter();
+          assert.equal(name, "Dogie");
+          assert.equal(symbol, "DOG");
+          assert.equal(counter, 0);
+        });
+        it("returns correct token URI", async () => {
+          const id = await basicNft.getTokenCounter();
+          const response = await basicNft.tokenURI(id);
+          assert.equal(response, await basicNft.TOKEN_URI());
+        });
+      });
+      describe("It allows users to mint NFT and updates the contract", async () => {
+        const tx = await basicNft.mintNFT();
+        await tx.wait(1);
+        const tokenCounter = await basicNft.getTokenCounter();
+        assert(tokenCounter, 1);
+        assert(tokenURI);
+      });
+    });
