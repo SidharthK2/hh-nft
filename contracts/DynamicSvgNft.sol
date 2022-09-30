e0f891a4a09b3c2e7251ed78311025de313481af
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-30 20:56:36 +0530

    update svg nft

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -4,13 +4,26 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721 {
     uint256 private s_tokenCounter;
+    string private i_lowImageUri;
+    string private i_highImageUri;
+    string private constant base64EncodedSvgPrefix =
+        "data:image/svg+xml;base64,";
 
-    constructor() ERC721("Dynamic SVG NFT", "DYN") {
+    constructor(string memory lowSvg, string memory highSvg)
+        ERC721("Dynamic SVG NFT", "DYN")
+    {
         s_tokenCounter = 0;
     }
+
+    function svgToImageURI(string memory svg)
+        public
+        pure
+        returns (string memory)
+    {}
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
