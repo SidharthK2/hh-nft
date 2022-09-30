301615882aec8e72e4ce13be099abd59daf584f2
SidharthK2 <sidharthkapoor.100@gmail.com>
2022-09-30 11:57:48 +0530

    fix requestNft unit test

--- test/randomIpfsNft.test.js
+++ test/randomIpfsNft.test.js
@@ -9,19 +9,39 @@ const { deploy, log } = deployments;
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("random Ipfs Nft", () => {
-      let randomIpfsNft, vrfCoordinatorV2Mock, deployer;
+      let randomIpfsNft, vrfCoordinatorV2Mock;
       beforeEach(async () => {
-        accounts = await ethers.getSigners();
-        deployer = accounts[0];
+        let { deployer } = await getNamedAccounts();
         await deployments.fixture(["mocks", "randomipfs"]);
-        randomIpfsNft = await ethers.getContract("RandomIpfsNft");
-        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+        randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer);
+        vrfCoordinatorV2Mock = await ethers.getContract(
+          "VRFCoordinatorV2Mock",
+          deployer
+        );
       });
       describe("requestNft", () => {
-        it("fails if payment isn't sent with the request", async function () {
-          await expect(randomIpfsNft.requestNft()).to.be.revertedWith(
+        it("fails if payment isn't sent with the request", async () => {
+          await expect(
+            randomIpfsNft.requestNft()
+          ).to.be.revertedWithCustomError(
+            randomIpfsNft,
             "RandomIpfsNft__NeedMoreETHSent"
           );
+        });
+        it("fails if payment is less than mint fee", async () => {
+          const mintFee = await randomIpfsNft.getMintFee();
+          await expect(
+            randomIpfsNft.requestNft({ value: mintFee / 10 })
+          ).to.be.revertedWithCustomError(
+            randomIpfsNft,
+            "RandomIpfsNft__NeedMoreETHSent"
+          );
+        });
+        it("emit event on success", async () => {
+          const mintFee = await randomIpfsNft.getMintFee();
+          await expect(
+            randomIpfsNft.requestNft({ value: mintFee.toString() })
+          ).to.emit(randomIpfsNft, "NftRequested");
         });
       });
     });
